6af0e5881be13f7a6a08956203edbaa80aa33200
penndev <pennilessfor@gmail.com>
2023-08-30 23:15:05 +0800

    add the http

--- main.go
+++ main.go
@@ -1,26 +1,47 @@
 package main
 
 import (
+	"net/http"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 type Cache struct {
-	Path     string
-	Size     int
-	Accessed int
-	Expried  int
+	Path     string `gorm:"primaryKey;comment:文件存储路径"`
+	Size     int    `gorm:"comment:文件大小"`
+	SiteID   int    `gorm:"comment:所属网站"`
+	Accessed int64  `gorm:"comment:访问时间lru用"`
+	Expried  int64  `gorm:"comment:过期时间"`
 }
 
-func main() {
-	db, err := gorm.Open(sqlite.Open(".data"), &gorm.Config{})
+var db *gorm.DB
+var err error
+
+func initData() {
+	// 创建数据库
+	db, err = gorm.Open(sqlite.Open(".data"), &gorm.Config{})
 	if err != nil {
 		panic("创建缓存数据库失败")
 	}
 	db.AutoMigrate(&Cache{})
+}
 
-	db.Create(&Cache{
-		Path: "123",
+func initServe() {
+
+	http.HandleFunc("/cached", func(w http.ResponseWriter, r *http.Request) {
+		r.PostForm.Get("")
 	})
+
+	// 启动HTTP服务器并监听本地端口8080
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	initData()
+	go initServe()
 
 }
