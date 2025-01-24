7fa610b7717d3851ecbdda27c996929c9aaaf644
penndev <pennilessfor@gmail.com>
2025-01-24 22:11:27 +0800

    add rust

---
+++ node/target/debug/deps/node.d
@@ -0,0 +1,5 @@
+C:\Users\Penn\Dev\github\wafcdn\node\target\debug\deps\node.exe: src\main.rs
+
+C:\Users\Penn\Dev\github\wafcdn\node\target\debug\deps\node.d: src\main.rs
+
+src\main.rs:
---
+++ node/target/debug/node.d
@@ -0,0 +1,1 @@
+C:\Users\Penn\Dev\github\wafcdn\node\target\debug\node.exe: C:\Users\Penn\Dev\github\wafcdn\node\src\main.rs
