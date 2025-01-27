72c76daeca8b988d82f3fd39940550361d490d8e
penndev <pennilessfor@gmail.com>
2025-01-27 23:52:02 +0800

    debug pingora

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,4 +1,9 @@
+use async_trait::async_trait;
+use pingora::prelude::*;
+use std::sync::Arc;
+
 fn main() {
- 
-    println!("Hi WafCdn");
+    let mut my_server = Server::new(None).unwrap();
+    my_server.bootstrap();
+    my_server.run_forever();
 }
