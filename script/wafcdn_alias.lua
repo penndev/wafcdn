9b33836937317e89d86ad984cef37621547441ca
penndev <pennilessfor@gmail.com>
2025-03-11 22:15:03 +0800

    add dev file

---
+++ script/wafcdn_alias.lua
@@ -0,0 +1,34 @@
+local ngx = require("ngx")
+local util = require("module.util")
+
+local WAFCDN_ALIAS = {}
+
+-- 静态文件目录访问
+function WAFCDN_ALIAS.rewrite()
+    -- 用户直接输入访问 /@static
+    if ngx.var.wafcdn_alias == "" then
+        ngx.exec("/rewrite"..ngx.var.uri)
+        return
+    end
+
+    local alias, _ = util.json_decode(ngx.var.wafcdn_alias)
+    if alias == nil then
+        util.status(500, "INTERNAL_ALIAS_JSON_FAIL")
+        return
+    end
+    -- 静态文件
+    ngx.var.wafcdn_alias_file = alias.file
+end
+
+
+function WAFCDN_ALIAS.header_filter()
+    if ngx.var.wafcdn_header == "" then
+        return
+    end
+    local header, _ = util.json_decode(ngx.var.wafcdn_header)
+    for key, val in pairs(header or {}) do
+        ngx.header[key] = val
+    end
+end
+
+return WAFCDN_ALIAS
